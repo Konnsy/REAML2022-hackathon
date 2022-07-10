455012ba65dac11bcb5b3e281ab72ec7153ef3ab
Konstantin <konstantin.wuestefeld@tu-dortmund.de>
2022-07-10 15:00:07 +0200

    adding the option of choosing the calculating device with the preprocessing transform

--- provided_code/preproc_transform.py
+++ provided_code/preproc_transform.py
@@ -1,17 +1,29 @@
-"""
-required library: torch (https://pytorch.org/get-started/locally/)
-"""
 import torch
 
 class PreprocessingTransform(object):
     """
-    A transform to help modify the input data
-    input:  a Block of images(frames)
-                shape: (1, BlockSize, X, Y)
-    output: a single image that represents the change over time for each pixel
-                shape: (1, X, Y)
+    A transform to calculate a preprocessed image from a block of images.
     """
-    def __call__(self, sample):        
+    def __init__(self, device=None):
+        """
+        device: device to execute calculations on (using the original device where 
+                    the data is located on before the transform takes place)
+        """
+        self.device = device
+
+
+    def __call__(self, sample):
+        """
+        input:  
+            sample: a block of frames of length w
+                        shape: (1, w, X, Y)
+
+            output: a single image that represents the change over time for each pixel
+                        shape: (1, X, Y)
+        """
+        if self.device is not None:
+            sample = sample.to(self.device)
+
         w = sample.shape[1]//2
         q1 = torch.mean(sample[:, :w, :, : ], dim=1)
         q2 = torch.mean(sample[:, w:, :, : ], dim=1)
