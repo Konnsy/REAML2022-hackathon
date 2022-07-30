577990ded30c40945c43d0da08b278b46545cbd4
Konstantin <konstantin.wuestefeld@tu-dortmund.de>
2022-07-30 15:52:42 +0200

    updating the paths fitting to new datasets

--- provided_code/task1_code_frame/data_paths.py
+++ provided_code/task1_code_frame/data_paths.py
@@ -2,23 +2,27 @@
 def get_pos_train_sets():
     return [
         "dataset_pos/part1",
-        #"dataset_pos/part2"
+        "dataset_pos/part2",
+        "dataset_pos/part3",
         ]
 
 def get_neg_train_sets():
     return [
         "dataset_neg/bg1",
-        #"dataset_neg/bg2"
+        "dataset_neg/bg2",
+        "dataset_neg/bg3",
         ]
 
 def get_pos_val_sets():
     return [
-        "dataset_pos_test/part1",
-        #"dataset_pos_test/part2"
+        "dataset_pos_val/part1",
+        "dataset_pos_val/part2",
+        "dataset_pos_val/part3",
             ]
 
 def get_neg_val_sets():
     return [
-        "dataset_neg_test/bg_set_1",
-        #"dataset_neg_test/bg_set_2",
+        "dataset_neg_val/bg1",
+        "dataset_neg_val/bg2",
+        "dataset_neg_val/bg3",
             ]
